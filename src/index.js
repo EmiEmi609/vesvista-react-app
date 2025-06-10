1fff454318a29666b23f2975d2eedc5db9c5a26e
EmiEmi609 <emilio.g.cuenca@gmail.com>
2025-06-10 18:57:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import VesVistaStore from './VesVistaStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <VesVistaStore />
-  </React.StrictMode>
-);
+const products = [
+  {
+    id: 1,
+    name: "Ves-Vista Sunglasses",
+    price: "$120",
+    description: "Polarized, fashion-forward shades with customizable lenses.",
+    image: "https://images.unsplash.com/photo-1589820296155-b59a3f00d3e8"
+  },
+  {
+    id: 2,
+    name: "Ves-Vista Sweater",
+    price: "$95",
+    description: "Premium cotton-blend sweater with minimalist Ves-Vista logo.",
+    image: "https://images.unsplash.com/photo-1618354691301-79840dd29d9e"
+  }
+];
